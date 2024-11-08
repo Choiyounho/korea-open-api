e02afc6f76cd69d455885e90cec974ffa20eb258
Choiyounho <maxosa78@gmail.com>
2024-11-09 01:29:17 +0900

    feat: MovieDao

---
+++ app/src/main/java/com/soten/openapi/data/local/db/dao/MovieDao.kt
@@ -0,0 +1,19 @@
+package com.soten.openapi.data.local.db.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.soten.openapi.data.local.db.entity.MovieEntity
+
+@Dao
+interface MovieDao {
+
+    @Query("SELECT * FROM movieentity")
+    suspend fun getAllMovies(): List<MovieEntity>
+
+    @Insert
+    suspend fun insertMovie(movieEntity: MovieEntity)
+
+    @Query("DELETE FROM movieentity WHERE code = :code")
+    suspend fun deleteMovie(code: String)
+}
