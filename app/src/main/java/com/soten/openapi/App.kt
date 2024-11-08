0dd2673fb053e0a80fcb3e8eef25bff1c331609b
Choiyounho <maxosa78@gmail.com>
2024-11-09 01:03:13 +0900

    feat: set hilt

---
+++ app/src/main/java/com/soten/openapi/App.kt
@@ -0,0 +1,7 @@
+package com.soten.openapi
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application()
--- app/src/main/java/com/soten/openapi/ui/main/MainActivity.kt
+++ app/src/main/java/com/soten/openapi/ui/main/MainActivity.kt
@@ -1,9 +1,10 @@
-package com.soten.openapi
+package com.soten.openapi.ui.main
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,7 +13,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.soten.openapi.ui.theme.OpenApiTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,10 +23,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             OpenApiTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "OpenApi Project",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    MainScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
