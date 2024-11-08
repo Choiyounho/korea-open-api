575bf629babb3ec597269a439d3ff1f0c04729b1
Choiyounho <maxosa78@gmail.com>
2024-11-09 01:23:10 +0900

    feat: ApiInfo

--- app/src/main/java/com/soten/openapi/data/remote/api/ApiInfo.kt
+++ app/src/main/java/com/soten/openapi/data/remote/api/ApiInfo.kt
@@ -1,4 +1,10 @@
 package com.soten.openapi.data.remote.api
 
 object ApiInfo {
+    const val BASE_URL = "http://www.kobis.or.kr/kobisopenapi/"
+    const val KEY = "7ec1bb57c28eb56fb8bd2c64aac6bb2e"
+
+    object EndPoints {
+        const val SEARCH = "webservice/rest/movie/searchMovieList.json"
+    }
 }
