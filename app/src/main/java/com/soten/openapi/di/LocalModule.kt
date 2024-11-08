96185d33820a5e6cf00d0fd80418cfb8e10de35b
Choiyounho <maxosa78@gmail.com>
2024-11-09 01:29:30 +0900

    feat: LocalModule

--- app/src/main/java/com/soten/openapi/di/LocalModule.kt
+++ app/src/main/java/com/soten/openapi/di/LocalModule.kt
@@ -1,4 +1,28 @@
 package com.soten.openapi.di
 
-class LocalModule {
+import android.content.Context
+import androidx.room.Room
+import com.soten.openapi.data.local.db.AppDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object LocalModule {
+
+    private const val DB_NAME = "APP_DATABASE_NAME"
+
+    @Singleton
+    @Provides
+    fun providesAppDatabase(@ApplicationContext context: Context) =
+        Room.databaseBuilder(context, AppDatabase::class.java, DB_NAME).build()
+
+    @Singleton
+    @Provides
+    fun providesMovieDao(appDatabase: AppDatabase) = appDatabase.movieDao()
+
 }
