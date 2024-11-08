c4fb4994c49bb61ef869b5b52c037d1a3eee4f97
Choiyounho <maxosa78@gmail.com>
2024-11-09 01:23:30 +0900

    feat: ApiInterceptor

--- app/src/main/java/com/soten/openapi/data/remote/interceptor/ApiInterceptor.kt
+++ app/src/main/java/com/soten/openapi/data/remote/interceptor/ApiInterceptor.kt
@@ -1,4 +1,18 @@
 package com.soten.openapi.data.remote.interceptor
 
-class ApiInterceptor {
+import com.soten.openapi.data.remote.api.ApiInfo
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class ApiInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response = with(chain) {
+        val newRequest = request().newBuilder()
+            .addHeader(APP_KEY, ApiInfo.KEY)
+            .build()
+        proceed(newRequest)
+    }
+
+    companion object {
+        private const val APP_KEY = "appKey"
+    }
 }
