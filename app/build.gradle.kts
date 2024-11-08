0e3b0d2be683508349108482afb06fa0bc7e9ffc
Choiyounho <maxosa78@gmail.com>
2024-11-09 00:59:00 +0900

    docs: 필요 라이브러리 주입

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
@@ -41,14 +43,21 @@ android {
 
 dependencies {
 
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.navigation)
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -56,4 +65,18 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.androidx.lifecycle.viewModel)
+    implementation(libs.androidx.lifecycle.savedState)
+
+    implementation(libs.hilt.navigation.compose)
+
+    implementation(libs.androidx.compose.paging)
+
+    implementation(libs.roomKtx)
+    implementation(libs.room)
+    ksp(libs.roomCompiler)
+
+    implementation(libs.bundles.retrofit)
+    implementation(libs.okhttp3.logging.interceptor)
 }
--- app/src/main/java/com/soten/openapi/MainActivity.kt
+++ app/src/main/java/com/soten/openapi/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             OpenApiTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "OpenApi Project",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
