02506792c2f4fa4035a3a77c94666dc7880528bc
Choiyounho <maxosa78@gmail.com>
2024-11-10 17:54:48 +0900

    feat: AppDatabase RemoteKey 적용

--- app/src/main/java/com/soten/openapi/data/local/db/AppDatabase.kt
+++ app/src/main/java/com/soten/openapi/data/local/db/AppDatabase.kt
@@ -3,10 +3,13 @@ package com.soten.openapi.data.local.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.soten.openapi.data.local.db.dao.MovieDao
+import com.soten.openapi.data.local.db.dao.RemoteKeyDao
 import com.soten.openapi.data.local.db.entity.MovieEntity
+import com.soten.openapi.data.local.db.entity.RemoteKey
 
-@Database(entities = [MovieEntity::class], version = 1)
-abstract class AppDatabase: RoomDatabase() {
+@Database(entities = [MovieEntity::class, RemoteKey::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
 
     abstract fun movieDao(): MovieDao
+    abstract fun remoteKeyDao(): RemoteKeyDao
 }
