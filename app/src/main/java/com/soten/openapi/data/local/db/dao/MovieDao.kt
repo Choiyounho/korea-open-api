f9b69908c4a38a3e07d160506edbe524e58b5a2b
Choiyounho <maxosa78@gmail.com>
2024-11-10 17:53:44 +0900

    refactor: MovieDao

--- app/src/main/java/com/soten/openapi/data/local/db/dao/MovieDao.kt
+++ app/src/main/java/com/soten/openapi/data/local/db/dao/MovieDao.kt
@@ -1,22 +1,28 @@
 package com.soten.openapi.data.local.db.dao
 
+import androidx.paging.PagingSource
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.soten.openapi.data.local.db.entity.MovieEntity
 
 @Dao
 interface MovieDao {
 
-    @Query("SELECT * FROM movieentity")
-    suspend fun getAllMovies(): List<MovieEntity>
-
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertMovie(movieEntity: MovieEntity)
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertMovies(movies: List<MovieEntity>)
 
     @Query("DELETE FROM movieentity WHERE code = :code")
     suspend fun deleteMovie(code: String)
+
+
+    @Query("DELETE FROM movieentity")
+    suspend fun clearAll()
+
+    @Query("SELECT * FROM movieentity")
+    fun getPagedMovies(): PagingSource<Int, MovieEntity>
 }
